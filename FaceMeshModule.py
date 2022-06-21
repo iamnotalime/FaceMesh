f05d87bf4c908e9648b2c6fe24ebadc5f7d7184f
EfanShinta <86037038+EfanShinta@users.noreply.github.com>
2022-06-21 08:07:10 +0700

    first commit

---
+++ FaceMeshModule.py
@@ -0,0 +1,59 @@
+import cv2
+import mediapipe as mp
+import time
+
+class FaceMeshDetector():
+    def __init__(self, staticMode = False, maxFaces = 2, minDetectionCon = 0.5, minTrackCon = 0.5):
+        self.staticMode = staticMode
+        self.maxFaces = maxFaces
+        self.minDetectionCon = minDetectionCon
+        self.minTrackCon = minTrackCon
+        
+        self.mpFaceMesh = mp.solutions.face_mesh
+        self.mpDraw = mp.solutions.drawing_utils
+        self.faceMesh = self.mpFaceMesh.FaceMesh(self.staticMode, self.maxFaces, self.minDetectionCon, self.minTrackCon)
+        self.drawSpec = self.mpDraw.DrawingSpec(thickness=2, circle_radius=5)
+
+    def findFaceMesh(self, img, draw = True): 
+        self.imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.faceMesh.process(self.imgRGB)
+        faces = []
+        if self.results.multi_face_landmarks: 
+            # faces = []
+            for faceLms in self.results.multi_face_landmarks:
+              if draw:
+                self.mpDraw.draw_landmarks(img, faceLms, self.mpFaceMesh.FACE_CONNECTIONS, self.drawSpec, self.drawSpec)
+              face = []
+              for id,lm in enumerate(faceLms.landmark):
+                    # print(lm)
+                    ih, iw, ic = img.shape
+                    x, y = int(lm.x*iw), int(lm.y*ih)
+                    # cv2.putText(img, str(id), (x,y), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,0), 1)
+                    # print(id,x,y)
+                    face.append([x,y])
+              faces.append(face)       
+        return img, faces 
+
+#First of all, we have to  initialize the value of the fps first before going ahead to the next
+
+#initial call
+def main():
+    cap = cv2.VideoCapture('Image/1.mp4')
+    pTime = 0
+    detector = FaceMeshDetector(maxFaces=2)
+    while True:
+        success, img = cap.read()
+        img, faces = detector.findFaceMesh(img)
+        if len(faces) != 0 :
+            print(faces[0])  
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS:{int(fps)}', (20,70), cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 3)
+        cv2.imshow("image", img)
+        cv2.waitKey(1)
+
+
+
+if __name__ == "__main__":
+    main()
